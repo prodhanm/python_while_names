f7c13f1fbaba409eef2b8bf47f6fb90373112dfc
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-16 16:11:57 -0400

    refactored the code to only use 'while = True'.

--- names.py
+++ names.py
@@ -1,8 +1,7 @@
-name_loop = True
 names = []
 
-def name_li(name_loop, names):
-    while name_loop != '-1':
+def name_li(names):
+    while True:
         name = input("What is your name: ")
         names.append(name)
         add_name = input("Would you like to continue. 'yes' \
@@ -12,7 +11,7 @@ def name_li(name_loop, names):
     return names
         
 def main():
-    for index, name in enumerate(name_li(name_loop,names), 1):
+    for index, name in enumerate(name_li(names), 1):
         print(f"{index}. {name}")
 
 if __name__ == "__main__":
