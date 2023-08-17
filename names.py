83e1b91fab8cf4446f9565b0434fdbd24968bca5
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-17 10:39:07 -0400

    slightly modified code.

--- names.py
+++ names.py
@@ -4,7 +4,6 @@ def name_li(family):
     while True:
         family_rel = input("What is the family association: ")
         name = input("What is your name: ")
-        #family.append(name)
         add_name = input("Would you like to continue? 'yes' \
                          or 'no': ")
         if add_name == 'no':
