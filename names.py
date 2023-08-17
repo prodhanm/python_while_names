d2b0ae6d2a861482af8736b5c2e503d7fcdd0a30
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-17 09:57:48 -0400

    transformed a list of family names into a dictionary.

--- names.py
+++ names.py
@@ -1,18 +1,21 @@
-names = []
+family = {}
 
-def name_li(names):
+def name_li(family):
     while True:
+        family_rel = input("What is the family association: ")
         name = input("What is your name: ")
-        names.append(name)
-        add_name = input("Would you like to continue. 'yes' \
+        #family.append(name)
+        add_name = input("Would you like to continue? 'yes' \
                          or 'no': ")
         if add_name == 'no':
             break
-    return names
+        family[family_rel] = name
+    return family
         
 def main():
-    for index, name in enumerate(name_li(names), 1):
-        print(f"{index}. {name}")
+    #for index, name in enumerate(name_li(names), 1):
+    #    print(f"{index}. {name}")
+    print(name_li(family))
 
 if __name__ == "__main__":
     main()
